ea8e1c9916ee29d42abd28f5e087a9649e336745
Max-Tr <fincsameddp@gmail.com>
2025-06-08 23:03:09 +0300

    Настроено окружение для написания тестов. Добавлены Unit-тесты для PriorityTag и boardsSlice

---
+++ client/src/components/Layout/PriorityTag.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import PriorityTag from './PriorityTag';
+
+describe('PriorityTag', () => {
+	it('should render a High priority tag', () => {
+		render(<PriorityTag priority="High" />);
+		const tagElement = screen.getByText('High');
+		expect(tagElement).toBeInTheDocument();
+		// Antd добавляет классы типа ant-tag-red
+		expect(tagElement.className).toContain('ant-tag-red');
+	});
+
+	it('should render a Medium priority tag', () => {
+		render(<PriorityTag priority="Medium" />);
+		const tagElement = screen.getByText('Medium');
+		expect(tagElement).toBeInTheDocument();
+		expect(tagElement.className).toContain('ant-tag-orange');
+	});
+
+	it('should render a Low priority tag', () => {
+		render(<PriorityTag priority="Low" />);
+		const tagElement = screen.getByText('Low');
+		expect(tagElement).toBeInTheDocument();
+		expect(tagElement.className).toContain('ant-tag-green');
+	});
+});
